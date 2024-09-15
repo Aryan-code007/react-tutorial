a73d1d079de2856530a0aa582cdbbe73da428f4d
Aryan-code007 <nadimpalli.aryan@gmail.com>
2024-09-15 18:35:42 +0530

    Working on React Components

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,16 +1,20 @@
-import { useState } from 'react';
+import './SubscribeForm.css';
+import React from 'react'
 
-export default function MyComponent() {
-  const [count, setCount] = useState(0);
 
+const MyComponent = () => {
   return (
-    <div>
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}>
-        Clicks: {count}
-      </button>
+    <div className="body">
+      <h2>SUBSCRIBE</h2>
+      <p>Sign up with your email address to receive news and updates.</p>
+      <form className="subscribe-form">
+        <input type="text" placeholder="First name" />
+        <input type="text" placeholder="Last name" />
+        <input type="email" placeholder="Email" /> <br></br>
+        <button type="submit">Subscribe</button>
+      </form>
     </div>
   );
-}
+};
+
+export default MyComponent;
